ab42bb49310404e87316fc80ffcbe5bf3e8e89f3
congbai <congbai@qunhemail.com>
2021-08-03 19:57:58 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,40 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base:'/feblog/',
-  themeConfig: {
-    nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Book', link: '/book/' },
-    ],
-    sidebar: {
-        '/book/':[{
-            title: 'Group 1', // 必要的
-            path: '/book/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: false, // 可选的, 默认值是 true,
-            sidebarDepth: 1, // 可选的, 默认值是 1
-            children: [['book1','书本']],
-        },{
-          title: 'Group 2', // 必要的
-          path: '/book/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          collapsable: false, // 可选的, 默认值是 true,
-          sidebarDepth: 1, // 可选的, 默认值是 1
-          children: [['book1','书本']],
-      }],
-        '': [['/','首页']],
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    base: '/feblog/',
+    themeConfig: {
+        nav: [
+            { text: 'Coding集', link: '/' },
+            { text: '披星集', link: '/book/' },
+            { text: '荷锄集', link: '/atrcile/' },
+            { text: '锄月集', link: '/han/' },
+        ],
+        sidebar: {
+            '/book/': [
+                {
+                    title: 'Group 1', // 必要的
+                    path: '/book/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1, // 可选的, 默认值是 1
+                    children: [['book1', '书本']],
+                },
+                {
+                    title: 'Group 2', // 必要的
+                    path: '/book/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1, // 可选的, 默认值是 1
+                    children: [['book1', '书本']],
+                },
+            ],
+            '': [
+                {
+                    title: 'JS工作机制', // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1, // 可选的, 默认值是 1
+                    children: [['/howjswork/1', '第一章']],
+                },
+            ],
+        },
+        search: false,
     },
-    search: false,
-},
-}
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,6 @@
-'# Hello VuePress' 
+# 前端技术讨论
+  - JS工作机制（翻译自How JS Work）
+  - React 实践
+  - 软件工程
+  - 前端工程建设
+  
